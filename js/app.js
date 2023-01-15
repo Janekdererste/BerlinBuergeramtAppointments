2d1093e8bb0fd99556b838a6c7530f10c0196a14
Janekdererste <janek.laudan@mailbox.org>
2023-01-15 11:32:43 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+console.log('Notifier loaded');
+
+// put this into the end of the event queue until all background scripts are loaded
+setTimeout(() => {
+
+        let elements = document.getElementsByClassName("buchbar");
+
+        // there is one example element which shows the bookable color. So we need more than that one element on the page
+        if (elements && elements.length && elements.length > 1) {
+            console.log((elements.length - 1) + " bookable appointments");
+            chrome.runtime.sendMessage('', {
+                type: 'notification',
+                options: {
+                    title: 'An appointment',
+                    message: 'There is ' + (elements.length - 1) + ' appointments',
+                    iconUrl: '/icon.png',
+                    type: 'basic'
+                }
+            });
+        }
+    }
+    , 0);
+
+// reload after 1min so that we don't get suspended.
+setTimeout(() => window.location.reload(), 60000)
+console.log("Reloading in 60s. ")
